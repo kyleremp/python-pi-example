d2ddbaea2b3d1711ae8db0394093282c16a0a940
Kyle <kyleremp@gmail.com>
2021-11-28 11:16:12 -0800

    Fixed comments, should've used # instead of //

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3.8
 # This is a sample Python script.
 def main(msg):
-    // No longer need comments
-    // New branch comment
+    # No longer need comments
+    # New branch comment
     print(msg)
+
 
 def secondary():
     print("new function")
+
 
 main("Hiya turkey butt!")
